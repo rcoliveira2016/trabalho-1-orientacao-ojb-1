aa37eb9ab868e97b683501da319d40d458e38df8
Ramon Cardoso de Oliveira <roramon7@gmail.com>
2021-10-28 21:49:45 -0300

    asoghuc

--- Exercico4/Program.cs
+++ Exercico4/Program.cs
@@ -6,25 +6,66 @@ namespace Exercico4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var retangulo = new Retangulo() { 
+                Altura = 1,
+                Largura = 5,
+            };
+            var circulo = new Circulo()
+            {
+                Radio = 4,
+            };
+            var quadrado = new Quadrado()
+            {
+                Altura = 5,
+                Largura = 5,
+            };
+
+            Console.WriteLine($"Retangulo - Area: {retangulo.CalculaArea()}, Perimetro: {retangulo.CalculaArea()}.");
+            Console.WriteLine($"Circulo - Area: {circulo.CalculaArea()}, Perimetro: {circulo.CalculaArea()}.");
+            Console.WriteLine($"Quadrado - Area: {quadrado.CalculaArea()}, Perimetro: {quadrado.CalculaArea()}.");
         }       
     }
     public abstract class Forma
     {
         public abstract double CalculaPerimetro();
-        public virtual double Perimetro()
-        {
-            return this.CalculaPerimetro();
-        }
+        public abstract double CalculaArea();
     }
     public class Retangulo : Forma
     {
-        public double Lado { get; set; }
+        public double Largura { get; set; }
         public double Altura { get; set; }
+
+        public override double CalculaArea()
+        {
+            return Largura * Altura;
+        }
+
         public override double CalculaPerimetro()
         {
-            return Lado * Altura;
+            return ((2 * Largura) + (2 * Altura));
         }
+
+    }
+
+    public class Circulo : Forma
+    {
+        const double Pi = 3.14;
+        public double Radio { get; set; }
+
+        public override double CalculaArea()
+        {
+            return Pi * (Radio * Radio);
+        }
+
+        public override double CalculaPerimetro()
+        {
+            return 2 * Pi * Radio;
+        }
+
+    }
+
+    public class Quadrado : Retangulo
+    {
 
     }
 }
